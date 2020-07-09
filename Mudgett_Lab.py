a0401a67047811b680eaa23c8b624f0227664377
cameronburton <63176134+cameronburton@users.noreply.github.com>
2020-07-09 00:08:31 -0700

    Add files via upload

---
+++ Mudgett_Lab.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as pt
+
+st.write('Mudgett Lab SAR Research')
+
+gene_df = pd.read_excel('C:/Users/Cameron Burton/Downloads/Arabidopsis thaliana_Gene(1)')
+
+
+
